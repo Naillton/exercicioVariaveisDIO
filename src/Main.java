542ec260f09dbcf7d181d3366c726a555a965424
Naillton <nailton_junior@protonmail.com>
2022-09-09 12:10:28 -0300

    definindo vario tipos de variaveis

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,15 @@ public class Main {
     public static void main(String[] args) {
         int i;
         int I = 10;
+        byte cem = 100;
+        char d = 'd';
         final int valor = 500;
-        double nota = 1.5;
+        double nota = 1.5d;
+        float not = 10.0f;
         i = 20;
         String name = "Nailton";
+        boolean logic = true;
+        long numeroAlto = 1000000000;
 
         System.out.println("O valor de i e = "+ i);
         System.out.println("O valor de I e = "+ I);
