c5435f6cd24305d1f50669b7b4531b05a4223681
Naillton <nailton_junior@protonmail.com>
2022-09-09 11:53:46 -0300

    string variavel

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         final int valor = 500;
         double nota = 1.5;
         i = 20;
+        String name = "Nailton";
 
         System.out.println("O valor de i e = "+ i);
         System.out.println("O valor de I e = "+ I);
         System.out.println("O valor de valor e = "+ valor);
         System.out.println("O valor de nota e = "+ nota);
+        System.out.println("O valor de name e = "+ name);
     }
 }
