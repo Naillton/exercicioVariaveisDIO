71a4c53d28857430cae7e8f414ebd32dc17e58ca
Naillton <nailton_junior@protonmail.com>
2022-09-09 11:00:22 -0300

    definindo variaveis em java

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        int i;
+        int I = 10;
+        final int valor = 500;
+        double nota = 1.5;
+        i = 20;
+
+        System.out.println("O valor de i e = "+ i);
+        System.out.println("O valor de I e = "+ I);
+        System.out.println("O valor de valor e = "+ valor);
+        System.out.println("O valor de nota e = "+ nota);
+    }
+}
