ac3d3ebc69db52ca19aee74cbe1973f52fb50629
Naillton <nailton_junior@protonmail.com>
2022-09-12 10:35:06 -0300

    atribuino valores

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,27 @@ public class Main {
         String name = "Nailton";
         boolean logic = true;
         long numeroAlto = 1000000000;
-
+        for (int x = 0; x <= 10; x++) {
+            System.out.println("valor do loop "+x);
+        }
+        int sum = 10 + 10;
+        System.out.println("soma = "+sum);
+        int sub = 10 - 5;
+        System.out.println("subtracao = "+sub);
+        long mult = 100000L * 2L;
+        System.out.println("multiplicacao = "+mult);
+        int k = 10;
+        System.out.println("valor inicial de k "+ k);
+        int posP = k++;
+        System.out.println("valor adicionado pos k "+ k);
+        int preP = ++k;
+        System.out.println("valor adicionado pre k "+ k);
+        int posS = k--;
+        System.out.println("valor subtraido pos k "+ k);
+        int preS = --k;
+        System.out.println("valor subtraido pre k "+ k);
+        k += 10;
+        System.out.println("valor modificado de k "+ k);
         System.out.println("O valor de i e = "+ i);
         System.out.println("O valor de I e = "+ I);
         System.out.println("O valor de valor e = "+ valor);
